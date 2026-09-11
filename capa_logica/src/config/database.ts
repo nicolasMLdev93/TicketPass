f8107834d1f63a8685934dbae487f93d072ad55d
Nicolas Bauza <nicolasbauza21@gmail.com>
2026-09-10 22:10:23 -0300

    Se migraron las tablas a la base de datos y se cargó 'eventos' con el archivo 001-events.ts del seeders

--- capa_logica/src/config/database.ts
+++ capa_logica/src/config/database.ts
@@ -3,11 +3,11 @@ import "dotenv/config";
 
 const sequelize = new Sequelize(
   process.env.DB_NAME || "",
-  process.env.DB_USER || "root",
-  process.env.DB_PASSWORD || "",
+  process.env.DB_USER || "",
+  process.env.DB_PASSWORD,
   {
-    host: process.env.DB_HOST || "localhost",
-    port: Number(process.env.DB_PORT) || 3306,
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
     dialect: "mysql",
     logging: false,
   },
